af03d6bb8489ff9842edac0cee44429f5ff9e4aa
Kunal <ksevkani495@gmail.com>
2020-06-23 09:08:51 +0530

    Updates

---
+++ precache-manifest.cd0fe2f300f6345a54b48f08681de31a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3266ba610b197efbf49f07239efdcb4c",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "83c649ead8bc22741c59",
+    "url": "/to-do-app/static/css/main.66081f9c.chunk.css"
+  },
+  {
+    "revision": "2b280f6555766f4ede43",
+    "url": "/to-do-app/static/js/2.1ce50f65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app/static/js/2.1ce50f65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c649ead8bc22741c59",
+    "url": "/to-do-app/static/js/main.5d2f27c7.chunk.js"
+  },
+  {
+    "revision": "65b486b8e64cf958fc45",
+    "url": "/to-do-app/static/js/runtime-main.e5e5607f.js"
+  }
+]);
